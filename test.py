bd385dbd2787c914f33ed99d879eab9e0f93d0f0
brice <brice.chauvat@isen-ouest.yncrea.fr>
2020-06-15 10:45:40 +0200

    commit

--- test.py
+++ test.py
@@ -6,10 +6,14 @@ def affichage_fichier_stl(lien) :
     figure= pyplot.figure()
     axes=mplot3d.Axes3D(figure)
     fichier=mesh.Mesh.from_file(lien)
-
+    a=(fichier.vectors)
+    print(a)
+    print(a[1])
+    print(len(a))
     axes.add_collection3d(mplot3d.art3d.Poly3DCollection(fichier.vectors))
     scale = fichier.points.flatten()
     axes.auto_scale_xyz(scale, scale, scale)
     pyplot.show()
 
-affichage_fichier_stl('support_casque.stl')
+
+affichage_fichier_stl('V_HULL.stl')
